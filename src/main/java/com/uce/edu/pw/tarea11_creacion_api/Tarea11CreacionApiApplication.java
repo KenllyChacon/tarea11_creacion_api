7887c4e47f55e8d3cfaf26056b1a5716dfd08676
Kenlly-Chacon <kevinleninchacon@hotmail.com>
2023-02-01 22:55:44 -0500

    Initial commit

---
+++ src/main/java/com/uce/edu/pw/tarea11_creacion_api/Tarea11CreacionApiApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu.pw.tarea11_creacion_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea11CreacionApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tarea11CreacionApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uce/edu/pw/tarea11_creacion_api/Tarea11CreacionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uce.edu.pw.tarea11_creacion_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea11CreacionApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
